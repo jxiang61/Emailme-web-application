d1743afecc7b5573d9b113086059eabc54243bc6
Junzhu <jzhsiang@gmail.com>
2020-05-07 21:31:14 -0400

    finished auth flow

--- services/passport.js
+++ services/passport.js
@@ -28,7 +28,8 @@ passport.use(new GoogleStrategy(
     {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback',
+        proxy: true //fix the http/https issue
     },
     (accessToken, refreshToken, profile, done) => {
         //the query returns a Promise
